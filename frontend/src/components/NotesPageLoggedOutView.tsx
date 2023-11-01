8f438e7d415ba56421c1ee5e7b71698023012fe5
saad-bhatti <saad.bhatti.cs@gmail.com>
2023-11-01 17:20:13 -0400

    pushing existing code

--- frontend/src/components/NotesPageLoggedOutView.tsx
+++
@@ -1,5 +0,0 @@
-const NotesPageLoggedOutView = () => {
-  return <p>Please login to see your notes.</p>;
-};
-
-export default NotesPageLoggedOutView;
--- frontend/src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(date: string): string {
-  return new Date(date).toLocaleString("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-  });
-}
